ac327a4fc6a9ea56afdc9d120712b991b6fc715a
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-07-15 18:06:44 +0100

    change: implement vite-plugin-fonts for performance

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Montserrat',
+            styles: 'wght@100;200;300;400;500;600;700;800;900',
+          },
+          {
+            name: 'Palanquin',
+            styles: 'wght@100;200;300;400;500;600;700',
+          },
+          {
+            name: 'Inter',
+            styles: 'wght@900',
+          },
+          {
+            name: 'Lato',
+            styles: 'ital,wght@0,900;1,900',
+          },
+          {
+            name: 'Noto Sans',
+            styles: 'wght@900',
+          }
+        ],
+        display: 'swap',
+      }
+    })
+  ],
   build: {
-    target: 'es2020' 
+    target: 'es2020'
   }
 })
