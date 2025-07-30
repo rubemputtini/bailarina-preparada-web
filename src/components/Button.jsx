7914093db417f412417896d1ca3e5651bb7d139f
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-07-30 19:27:08 +0100

    change: modify button redirection

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
+import { navLinks } from "../constants";
+
 const Button = ({ label }) => {
 
+    const hotmartLink = navLinks[2].href;
+
     return (
-        <a href="#cta" className="gap-2 px-3 py-1.5 md:py-2 border font-montserrat md:text-lg font-bold leading-none bg-[#6C5DD3] hover:bg-[#4a3f8d] rounded-full text-white">
+        <a href={hotmartLink} className="gap-2 px-3 py-1.5 md:py-2 border font-montserrat md:text-lg font-bold leading-none bg-[#6C5DD3] hover:bg-[#4a3f8d] rounded-full text-white">
             {label}
         </a>
     )
